6fd6a901d7cb77a9c2242a2e6c54f4a82f45145d
seoin1102 <im_in_@naver.com>
2022-04-26 18:26:45 +0900

    practice

--- java-study/practice02/src/main/java/prob02/Prob02.java
+++ java-study/practice02/src/main/java/prob02/Prob02.java
@@ -5,18 +5,24 @@ import java.util.Scanner;
 public class Prob02 {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+        Scanner in = new Scanner( System.in );
+        int intArray[] = new int[5];
+        double sum = 0;
 
-		int[] intArray = new int[ 5 ];
-		double sum = 0;
-
+        System.out.println("5개의 숫자를 입력하세요.");
 		/* 키보드에서 배열 크기만큼 입력 받아 배열에 저장하는 코드 */
-		
+		for(int i=0; i < intArray.length; i++) {
+			int num = in.nextInt();
 		/* 배열에 저장된 정수 값 더하기 */
+			sum += num;
+		}
+		
+		
 		
 		/* 출력 */
+		System.out.printf("평균은 %.1f 입니다.",sum/5);
 		
 		/* 자원정리 */
-		scanner.close();
+
 	}
 }
