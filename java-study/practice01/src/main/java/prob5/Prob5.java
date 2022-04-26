e2c44cf46a12b2e5ad5ac5a5b8b39c5597e2f3b0
seoin1102 <im_in_@naver.com>
2022-04-26 20:01:16 +0900

    practice

--- java-study/practice01/src/main/java/prob5/Prob5.java
+++ java-study/practice01/src/main/java/prob5/Prob5.java
@@ -6,28 +6,20 @@ public class Prob5 {
 		for(int num=1; num <= 99; num++) {
 			String num2 = Integer.toString(num);
 			if(num2.length() == 1 && num2.contains("3") || num2.length() == 1 && num2.contains("6") || num2.length() == 1 && num2.contains("9")) {
-				System.out.print(num2);
-				System.out.print(" ");
-				System.out.print("짝");
-				System.out.println("");
+				System.out.printf("%s 짝%n",num2);
 			}
 			
 			else if(num2.length() == 2) {
 				if(num2.indexOf("3") == 0 || num2.indexOf("6") == 0 || num2.indexOf("9") == 0) {
-					System.out.print(num2);
-					System.out.print(" ");
-					System.out.print("짝");
+					System.out.printf("%s 짝",num2);
 				if(num2.lastIndexOf("3") == 1 || num2.lastIndexOf("6") == 1 || num2.lastIndexOf("9") == 1) {
 					System.out.print("짝");
 						}System.out.println("");}
 			else if(num2.indexOf("3") == 1 || num2.indexOf("6") == 1 || num2.indexOf("9") == 1) {
-					System.out.print(num2);
-					System.out.print(" ");
-					System.out.print("짝");
+					System.out.printf("%s 짝",num2);
 				if(num2.lastIndexOf("3") == 0 || num2.lastIndexOf("6") == 0 || num2.lastIndexOf("9") == 0) {
 					System.out.print("짝");
 						}System.out.println("");}
-
 			}
 
 		}
