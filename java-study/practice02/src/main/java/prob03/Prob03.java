92c7c183e2cb8074607aebded2f481f7ea063026
seoin1102 <im_in_@naver.com>
2022-04-26 19:05:05 +0900

    practice

--- java-study/practice02/src/main/java/prob03/Prob03.java
+++ java-study/practice02/src/main/java/prob03/Prob03.java
@@ -6,12 +6,17 @@ public class Prob03 {
 
 		// 원래 배열 원소 출력
 		//printCharArray(c);
-
+		System.out.println(c);
 		// 공백 문자 바꾸기
 		//replaceSpace(c);
-
+		for(int i=0; i < c.length; i++) {
+			if(c[i]==' ') {
+				c[i] = ',';
+			}
+		}
 		// 수정된 배열 원소 출력
 		//printCharArray(c);
+		System.out.println(c);
 	}
 
 }
