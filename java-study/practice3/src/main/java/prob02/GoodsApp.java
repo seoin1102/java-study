57f51b0fe845efe7fa8901253ea11346b580e5a9
seoin1102 <69146576+seoin1102@users.noreply.github.com>
2022-04-27 23:39:32 +0900

    Update GoodsApp.java

--- java-study/practice3/src/main/java/prob02/GoodsApp.java
+++ java-study/practice3/src/main/java/prob02/GoodsApp.java
@@ -1,5 +1,6 @@
 package prob02;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GoodsApp {
@@ -8,22 +9,25 @@ public class GoodsApp {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		//Goods[] goods = new Goods[COUNT_GOODS];
-		for(int i = 0; i < COUNT_GOODS; i++) {
-			String line = scanner.nextLine();
-			System.out.println(line);
-			String[] datas = line.split(" ");
-			
-			String name = datas[0];
-			int price = Integer.parseInt(datas[1]);
-			int count = Integer.parseInt(datas[2]);
-			
-			System.out.println(name + ":" + price + ":" + count);
-		}
+		Goods[] goods = new Goods[COUNT_GOODS];
 
 		// 상품 입력
-
-		// 상품 출
+		for(int i = 0; i< COUNT_GOODS; i++) {
+			String line = scanner.nextLine();
+			String[] tokens = line.split(" ");
+			
+			String name = tokens[0];
+			int price = Integer.parseInt(tokens[1]);
+			int count = Integer.parseInt(tokens[2]);
+			
+			goods[i] = new Goods(name, price, count);
+			
+			
+		}
+		// 상품 출력
+		for (int i = 0; i< COUNT_GOODS; i++) {
+			goods[i].display();
+		}
 		
 		// 자원정리
 		scanner.close();
