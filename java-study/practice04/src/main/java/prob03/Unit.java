83f92408ded02a31f6d7bdd5dc4b2ad44d87581d
seoin <im_in_@burningb.com>
2022-04-28 22:50:54 +0900

    commit

---
+++ java-study/practice04/src/main/java/prob03/Unit.java
@@ -0,0 +1,15 @@
+package prob03;
+
+public class Unit {
+
+	void move(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
