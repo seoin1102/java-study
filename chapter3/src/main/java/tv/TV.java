856c7ecf4693497bd2d20969a5442967a0d7ea02
seoin <im_in_@burningb.com>
2022-04-28 23:18:11 +0900

    commit

--- chapter3/src/main/java/tv/TV.java
+++ chapter3/src/main/java/tv/TV.java
@@ -4,8 +4,14 @@ public class TV {
 	private int channel; // 0 ~ 255 rotation   
 	private int volume; // 0 ~ 100 rotation 제한
 	private boolean power;
+	public TV() {
+	}
 	
-	
+	public TV(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+	}
 	public int getChannel() {
 		return channel;
 	}
@@ -27,12 +33,14 @@ public class TV {
 	public void volume(boolean up) {
 		volume(this.volume+1);
 		volume(volume + (up ? 1: -1));
-		if (volume<0) {
-		}
 	}
 	public void volume(int volume) {
-		this.volume = volume;
-		if (volume<0) {
+		if (volume > 100) {
+			this.volume = volume % 100;
+		} else if (volume < 0) {
+			this.volume = 100 - Math.abs(volume);
+		} else {
+			this.volume = volume;
 		}
 	}
 	public void status() {
@@ -41,4 +49,29 @@ public class TV {
 						+ "volume = "+ volume +", "
 								+ "power="+ (power? "on" : "off")+"]");
 	}
+	public void power(boolean power) {
+		this.power = power;
+	}
+
+	
+
+	public void channel(int channel) {
+		if (channel > 255) {
+			this.channel = channel % 255;
+		} else if (channel < 0) {
+			this.channel = 255 - Math.abs(channel);
+		} else {
+			this.channel = channel;
+		}
+			
+	}
+	
+	public void channel(boolean up) {
+		channel(channel +=(up?1:-1));
+	}
+
+	
+	
+	
+
 }
