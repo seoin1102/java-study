68ce191d8e57c52a9ffb1ccd0f46dc3ec70aedf2
seoin1102 <im_in_@naver.com>
2022-04-28 17:42:21 +0900

    commit

--- java-study/practice04/src/main/java/prob02/Book.java
+++ java-study/practice04/src/main/java/prob02/Book.java
@@ -2,6 +2,59 @@ package prob02;
 
 public class Book {
 	private int bookNo;
+	private String title;
+	private String author;
 	private int stateCode = 1; //0: 대여중, 1: 재고 있음
+	
+	public int getBookNo() {
+		return bookNo;
+	}
+
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getStateCode() {
+		return stateCode;
+	}
+
+	public void setStateCode(int stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	public Book(int bookNo, String title, String author) {
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+		
+			}
+	
+	public void rent(int num) {
+		if(this.bookNo == num) {
+			this.stateCode = 0;
+			System.out.println(this.title+"이(가) 대여 됐습니다.");
+		}
+	}
+	
+	public void print() {
+		System.out.println("책 제목:" + title +", 작가:"+ author+", 대여 유무:"+(stateCode==0?"대여중":"재고있음"));
+
+	}
 
 }
