643ffc41592d7a40bcff4160b94411f0025c2337
seoin1102 <69146576+seoin1102@users.noreply.github.com>
2022-05-02 22:43:34 +0900

    commit

---
+++ java-study/chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,23 @@
+package chapter04;
+
+public class StringTest01 {
+	public static void main(String[] args) {
+		// c:\temp
+		System.out.println("c:\\temp");
+		
+		// "hello"
+		System.out.println("\"hello\"");
+		
+		// 제어문자
+		// \t: tab
+		// \r: carrige return
+		// \n: newline           
+		// \b: beep
+		System.out.print("hello\tworld\n");
+		System.out.println("hello\tworld");
+
+		// '
+		char c = '\'';
+		System.out.println(c);
+	}
+}
