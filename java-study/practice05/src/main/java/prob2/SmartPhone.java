d5f9807bfc05cc6937b6e751bc9e6042010fe45e
seoin1102 <69146576+seoin1102@users.noreply.github.com>
2022-05-02 22:52:17 +0900

    commit

--- java-study/practice05/src/main/java/prob2/SmartPhone.java
+++ java-study/practice05/src/main/java/prob2/SmartPhone.java
@@ -1,5 +1,21 @@
 package prob2;
 
 public class SmartPhone extends MusicPhone {
-	
+	@Override
+	public void execute(String function) {
+		if (function.equals("앱")) {
+			App();
+			return;
+		}
+		super.execute(function);
+	}
+
+	@Override
+	public void playMusic() {
+		System.out.println("다운로드해서 음악재생");
+	}
+
+	private void App() {
+		System.out.println("앱실행");
+	}
 }
