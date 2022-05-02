75db9f27c7539a19c17f0be0bd81a58447b46fca
seoin1102 <69146576+seoin1102@users.noreply.github.com>
2022-05-02 23:05:31 +0900

    commit

--- java-study/practice05/src/main/java/prob5/MyStack.java
+++ java-study/practice05/src/main/java/prob5/MyStack.java
@@ -1,19 +1,44 @@
 package prob5;
 
 public class MyStack {
+	private int top = 0;
 	private String[] buffer;
 	
 	public MyStack(int size) {
-		buffer = new String[size];
+		buffer = new String [size];
 	}
 	
 	public void push(String item) {
+		if(top == buffer.length) {
+			String[] temp = buffer;
+			buffer = new String [buffer.length*2];
+			for(int i = 0; i<temp.length; i++) {
+				buffer[i]  = temp[i];
+			}
+		}
+		buffer[top] = item;
+		top ++;
+	}
+	
+	public String pop() throws MyStackException{
+		if(top <0 ) {
+			throw new MyStackException();
+		}
 		
+		if(buffer[top] == null) {
+			top --;
+		}
+
+		String item = buffer[top];
+		buffer[top] = null ;
+		top --;
+		return item;
 	}
-	public String pop() {
-		return null;
-	}
+	
 	public boolean isEmpty() {
-		
+		if(top < 0)
+			return true;
+		else
+			return false;	
 	}
 }
