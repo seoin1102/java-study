dc8b07b18cbff6b5d9000cad8f85010b3ff097f0
seoin1102 <69146576+seoin1102@users.noreply.github.com>
2022-05-03 16:41:08 +0900

    commit

---
+++ java-study/chapter04/src/main/java/io/BufferedReaderTest.java
@@ -0,0 +1,46 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderTest {
+
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		
+		// 기반 스트림
+		try {
+			FileReader fr = new FileReader("./src/main/java/io/BufferedReaderTest.java");
+		// 보조 스트림
+		br = new BufferedReader(fr);
+		
+		int index = 0;
+		String line = null;
+		while((line = br.readLine()) != null) {
+			index++;
+			System.out.print(index);
+			System.out.print(":");
+			System.out.print(line);
+			System.out.println("");
+		}
+		} catch (FileNotFoundException e) {
+			System.out.println("Can't Open:" + e);
+			
+		}catch (IOException e) {
+			System.out.println("Error" + e);
+			
+		}finally {
+			try {
+				if(br !=null) {
+					br.close();
+				}
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
