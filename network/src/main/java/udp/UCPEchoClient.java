cf6b88e8e57021c41e677d45fea49249a3c9ae89
seoin1102 <im_in_@naver.com>
2022-05-09 11:46:03 +0900

    commit

--- network/src/main/java/udp/UCPEchoClient.java
+++ network/src/main/java/udp/UCPEchoClient.java
@@ -1,9 +1,62 @@
 package udp;
 
-public class UCPEchoClient {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.util.Scanner;
 
+public class UCPEchoClient {
+	private static final String SERVER_IP = "";
+	private static final int SERVER_PORT = UDPEchoServer.PORT;
+	private static final int BUFFER_SIZE = UDPEchoServer.BUFFER_SIZE;
+	
 	public static void main(String[] args) {
+		DatagramSocket socket = null;
+		Scanner scanner = null;
+		try {
+		// 1. Scanner 생성
+		scanner = new Scanner(System.in);
 		
+		// 2. Socket 생성
+		socket = new DatagramSocket();
+
+		while(true) {
+			System.out.println(">>");
+			String line = scanner.nextLine();
+			
+			if("exit".equals(line)) {
+				break;
+			
+		}
+		// 3. 보내기
+		byte[] sndData = line.getBytes("utf-8");
+		DatagramPacket sndPacket = new DatagramPacket(
+				sndData, 
+				sndData.length, 
+				new InetSocketAddress("127.0.0.1", SERVER_PORT));
+		socket.send(sndPacket);
+		
+		// 4. 받기
+		DatagramPacket rcvPacket = new DatagramPacket(new byte[BUFFER_SIZE], BUFFER_SIZE);
+		socket.receive(rcvPacket); // blocking
+		
+		byte[] rcvData= rcvPacket.getData();
+		int length = rcvPacket.getLength();
+		String message = new String(rcvData, 0, length, "UTF-8");
+		System.out.println("<<"+message);
+		}
+	}catch (IOException e) {
+			System.out.println("Error:" + e);
+		
+		}finally {
+			if(scanner != null) {
+				scanner.close();
+			}if(socket !=null) {
+				socket.close();
+			}
+		}
 	}
 
 }
